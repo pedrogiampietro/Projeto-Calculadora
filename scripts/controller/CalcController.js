1e466f2052fc23d5125c7e93af64d72aab4f3c90
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-05-16 20:16:50 -0300

    Adicionado EventListerAll, split e cursor.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -8,8 +8,9 @@ class CalcController {
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
 
-    }
+    };
 
     initialize(){
 
@@ -21,6 +22,36 @@ class CalcController {
 
         }, 1000);
 
+    };
+
+    addEventListenerAll(element, events, fn) {
+
+        events.split(' ').forEach(event => {
+
+            element.addEventListener(event, fn, false);
+
+        });
+
+    }
+
+    initButtonsEvents(){
+
+        let buttons = document.querySelectorAll("#buttons > g, #parts >g");
+
+        buttons.forEach((btn, index) =>  {
+
+            this.addEventListenerAll(btn, "click drag", e => {
+
+            console.log(btn.className.baseVal.replace("btn-",""));
+
+             });
+
+             this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+
+                btn.style.cursor = "pointer";
+
+             });
+        });
     }
 
     setDisplayDateTime() {
